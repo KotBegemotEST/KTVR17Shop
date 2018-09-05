ccae2efbbfc963f57a58ca26a8e05eacd0528e60
pupil <pupil@ivkhk.local>
2018-09-05 10:25:07 +0300

    create shop main

---
+++ src/shopktvr17/shopKTVR17.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shopktvr17;
+
+/**
+ *
+ * @author pupil
+ */
+public class shopKTVR17 {
+    public static void main(String[] args) {
+        
+    }
+}
